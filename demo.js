25c947b6e0415227bf9d58a7878d9ffeaff4671b
Jerome <jerome.neupert@epitech.eu>
2024-04-15 22:38:44 +0200

    base done revert to this if need me

--- demo.js
+++ demo.js
@@ -8,12 +8,19 @@ function createFloor(){
         new THREE.PlaneGeometry(100,100,10,10),
         new THREE.MeshBasicMaterial({color:0x444444, wireframe:false})
     );
+
+    // Position the floor half a unit below the player
+    floor.position.y = -.5;
+    floor.rotation.x = -Math.PI / 2;
+
     scene.add(floor);
 }
-
 function init(){
    scene = new THREE.Scene();
    camera = new THREE.PerspectiveCamera(90,1280/720,0.1,1000);
+
+   const gridHelper = new THREE.GridHelper(100, 10);
+   scene.add(gridHelper);
 
    mesh = new THREE.Mesh(
         new THREE.BoxGeometry(1,1,1),
@@ -28,7 +35,7 @@ function init(){
 
      createFloor();
 
-    camera.position.set(0,0,5);
+    camera.position.set(0,0,3);
     camera.lookAt(new THREE.Vector3(0,0,0));
     scene.add(camera);
 
@@ -47,21 +54,27 @@ function init(){
 
 
 function moveCube(event) {
+    let speed = 0.1;
     switch (event.key) {
-        case 'z':
-            mesh.position.y += 0.1;
-            break;
         case 's':
-            mesh.position.y -= 0.1;
+            mesh.position.z += speed;
+            camera.position.z += speed;
+            break;
+        case 'z':
+            mesh.position.z -= speed;
+            camera.position.z -= speed;
+
             break;
         case 'q':
-            mesh.position.x -= 0.1;
+            mesh.position.x -= speed;
+            camera.position.x -= speed;
             break;
         case 'd':
-            mesh.position.x += 0.1;
+            mesh.position.x += speed;
+            camera.position.x += speed;
             break;
     }
-    camera.position.set(mesh.position.x, mesh.position.y, camera.position.z);
+    camera.position.set(mesh.position.x, mesh.position.y+5, camera.position.z);
     camera.lookAt(mesh.position);
 }
 
